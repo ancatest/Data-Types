480ae4a6f1c5e7cc5eb4141c4e5e3749653140b2
ancatest <anca.canura@gmail.com>
2024-09-27 13:10:51 +0300

    testa1

--- E2E/sort.spec.ts
+++ E2E/sort.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("sortflogin", { tag: ["@smoke"],},  async ({ page }) => {
+test("sortflogin",async ({ page }) => {
 await page.goto("https://letcode.in/test");
 await expect(page.getByRole('heading',{name:'Practice and become pro in test automation'})).toBeVisible();
 await page.getByRole('link',{name:'AUI - 3'}).click();
